d40dc971591683b64adece24a3c1a13cd11bed81
ly26005 <liaoyi@liaoyideMacBook-Pro.local>
2015-10-22 21:44:22 +0800

    1st commit
    new create,learning objc

---
+++ objc_code_save/第一个类，分数的输入输出显示/main.m
@@ -0,0 +1,66 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by liaoyi on 15/10/20.
+//  Copyright © 2015年 liaoyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// @interface part
+@interface Fraction : NSObject
+-(void) print;
+-(void) setNumerator:(int) n;
+-(void) setDenominator:(int) d;
+-(int) numerator;
+-(int) denominator;
+@end
+
+// @implementation part
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+-(void) print
+{
+    NSLog(@"%i/%i",numerator,denominator);
+}
+
+-(void) setNumerator:(int)n
+{
+    numerator = n;
+}
+-(void) setDenominator:(int)d
+{
+    denominator = d;
+}
+
+-(int) numerator
+{
+    return numerator;
+}
+-(int) denominator
+{
+    return denominator;
+}
+
+@end
+
+//program part
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Fraction *myFraction = [[Fraction alloc] init];
+        
+        [myFraction setNumerator:1];
+        [myFraction setDenominator:3];
+        
+        NSLog(@"Fir:The value of myfraction is");
+        [myFraction print];
+        
+        NSLog(@"Sec:The value of myfraction is %i/%i",[myFraction numerator],[myFraction denominator]);
+    }
+    return 0;
+}
